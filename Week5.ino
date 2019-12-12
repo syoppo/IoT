29fdabb3cc509129693fe4f80bee96d49c1b5c5c
Jonghyun Kim <jhkim@dit.ac.kr>
2019-12-12 23:10:07 +0900

    add Week5.ino

---
+++ Week5.ino
@@ -0,0 +1,14 @@
+int ledPin = 7; // ledPin을 7번으로 정의
+
+void setup() {
+  pinMode(ledPin, OUTPUT);    //ledPin을 출력으로 설정
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); //LED ON
+  delay(100);                //1초 대기
+  digitalWrite(ledPin, LOW);  //LED OFF
+  delay(100);                //1초 대기
+}
+
+
